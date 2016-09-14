1b1774de15ab8eee75683276e6ac8df4c74f64e2
Ferris Thomas <fsthomas@gmail.com>
2016-09-14 11:08:04 -0400

    Updates style of email from contact form.

--- src/contact.php
+++ src/contact.php
@@ -9,6 +9,11 @@ if(count($_POST) === 0){
         $data[$key] = $value;
     }
     $data["error"] = 0;
+    $message = "FROM: " . $data['email'] . "\n" . "RE: " . $data['venue'] . "\n" . "Message:\n" . str_replace("\n.", "\n..", $data['message']);
+    $headers = 'From: info@southerngroundnashville.com' . "\r\n" .
+        'Reply-To: ' . $data[email] . "\r\n" .
+        'X-Mailer: PHP/' . phpversion();
+    $data["mail"] = mail ( "ferris@southernground.com", "SGN Contact Re: " + $data['venue'] , $message, $headers );
 }
 
 header('Content-type: application/json');
