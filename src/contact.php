c8b189995fc1f1e1cf00b7f872d9ac7be89dcad6
Ferris Thomas <fsthomas@gmail.com>
2017-09-28 11:06:08 -0400

    Removes BCC headers from the email generation. Adds secondary email for testing purposes.

--- src/contact.php
+++ src/contact.php
@@ -12,9 +12,9 @@ if(count($_POST) === 0){
     $message = "FROM: " . $data['email'] . "\n" . "COMPANY: " . $data['company'] . "\n" . "MESSAGE:\n\n" . str_replace("\n.", "\n..", $data['message']);
     $headers = 'From: info@southerngroundartists.com' . "\r\n" .
         'Reply-To: ' . $data['email'] . "\r\n" .
-        'BCC: info@southerngroundnashville.com, dev@southernground.com, ferris@southernground.com' . "\r\n" .
         'X-Mailer: PHP/' . phpversion();
     $data["mail"] = mail ( "info@southerngroundartists.com", "SGN Contact Email", $message, $headers );
+    $data["secondary"] = mail ( "ferris@southernground.com", "SGN Contact Email", $message, $headers );
 }
 
 header('Content-type: application/json');
